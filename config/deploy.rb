e7073434fedad34db9c94a747118bc32d1bb199e
Pete Gadomski <pete.gadomski@gmail.com>
2012-09-20 09:33:45 -0700

    Add deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,42 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+# config/deploy.rb
+require 'rvm/capistrano'
+set :rvm_ruby_string, '1.9.3'
+set :rvm_type, :system
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+# Bundler tasks
+require 'bundler/capistrano'
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :application, "rua.docbenton.org"
+set :repository,  "ssh://docbenton.org/home/peteg/git/reddingultimate.org.git"
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+set :scm, :git
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+set :use_sudo, false
+set(:run_method) { use_sudo ? :sudo : :run }
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+default_run_options[:pty] = true
+
+set :user, "peteg"
+set :group, user
+set :runner, user
+
+set :host, "#{user}@reddingultimate.org"
+role :web, host
+role :app, host
+
+set :deploy_to, "/srv/www/#{application}"
+set :unicorn_conf, "#{deploy_to}/current/config/unicorn.rb"
+set :unicorn_pid, "#{deploy_to}/shared/pids/unicorn.pid"
+
+# Unicorn control tasks
+namespace :deploy do
+  task :restart do
+    run "if [ -f #{unicorn_pid} ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{deploy_to}/current && bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D; fi"
+  end
+  task :start do
+    run "cd #{deploy_to}/current && bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D"
+  end
+  task :stop do
+    run "if [ -f #{unicorn_pid} ]; then kill -QUIT `cat #{unicorn_pid}`; fi"
+  end
+end
